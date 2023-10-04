c9d67ae9366fda90503da324a27ac4f2759dbdbe
Dhriti SIngh <md23e@Arif_Canada.wireless.concordia.ca>
2023-10-04 17:32:52 -0400

    changes made in GameEngine

--- Risk_Game/src/main/java/GameEngine.java
+++ Risk_Game/src/main/java/GameEngine.java
@@ -1,7 +1,8 @@
-import model.GameController;
 import model.GameCycle;
-import utils.InvalidExecutionException;
-import utils.ValidationException;
+import model.GameHandler;
+import utils.InvalidExecutionFailure;
+import utils.ValidationFailure;
+
 import java.util.Objects;
 
 /**
@@ -11,11 +12,11 @@ import java.util.Objects;
  * @version 1.0.0
  */
 public class GameEngine {
-    GameCycle d_GameCycle = GameCycle.MapEditor;
+    GameCycle d_GameCycle = GameCycle.MapBuilder;
 
     /**
      * Main method to run the game
-     * 
+     *
      * @param args passed to main if used in command line
      */
     public static void main(String[] args) {
@@ -24,24 +25,23 @@ public class GameEngine {
 
     /**
      * The function which runs the whole game in phases
-     *
      */
     public void start() {
         try {
             if (Objects.isNull(d_GameCycle)) {
                 throw new Exception("Game Phase is Null");
             }
-            if (Objects.nonNull(d_GameCycle) && !d_GameCycle.equals(GameCycle.ExitGame)) {
-                GameController l_GameController = d_GameCycle.getController();
+            if (Objects.nonNull(d_GameCycle) && !d_GameCycle.equals(GameCycle.GameExit)) {
+                GameHandler l_GameController = d_GameCycle.getController();
                 if (Objects.isNull(l_GameController)) {
                     throw new Exception("There is no controller");
                 }
-                d_GameCycle = l_GameController.start(d_GameCycle);
+                d_GameCycle = l_GameController.begin(d_GameCycle);
                 System.out.println(d_GameCycle + " Phase has been Entered");
                 System.out.println("-----------------------------------------------------------------------------------------");
                 start();
             }
-        } catch (ValidationException | InvalidExecutionException p_Exception) {
+        } catch (ValidationFailure | InvalidExecutionFailure p_Exception) {
             System.err.println(p_Exception.getMessage());
             start();
         } catch (Exception p_Exception) {
